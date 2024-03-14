cb87316cc2f9a593b175f4420ee0c36f6e8554d5
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-03-14 23:59:22 +0200

    Sixth commit

--- spring-in-action-6-samples-main/ch04/tacos-sd-mongodb/src/main/java/tacos/Ingredient.java
+++ spring-in-action-6-samples-main/ch04/tacos-sd-mongodb/src/main/java/tacos/Ingredient.java
@@ -8,13 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@Document
+@Data 
+@Document //Indicate that Ingredient a document entity that can be written to and read from a MongoDB
 @AllArgsConstructor
 @NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
 public class Ingredient {
 
-  @Id
+  @Id //Designates a property as the document ID 
   private String id;
   private String name;
   private Type type;
