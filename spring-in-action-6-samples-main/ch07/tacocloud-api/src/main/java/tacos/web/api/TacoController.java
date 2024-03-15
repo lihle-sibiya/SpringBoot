e252b007c86339a0eef1817e2d01ec9307bbbdee
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-03-16 00:05:56 +0200

    Ninth commit

--- spring-in-action-6-samples-main/ch07/tacocloud-api/src/main/java/tacos/web/api/TacoController.java
+++ spring-in-action-6-samples-main/ch07/tacocloud-api/src/main/java/tacos/web/api/TacoController.java
@@ -20,7 +20,7 @@ import tacos.data.TacoRepository;
 @RestController //REST controller
 @RequestMapping(path="/api/tacos",                      // <1>
                 produces="application/json")//JSON based API
-@CrossOrigin(origins="http://tacocloud:8080")        // <2> Apply CORS
+@CrossOrigin(origins="http://tacocloud:8080")        // <2> CORS allows clients from localhost:8080 to consume API
 public class TacoController {
   private TacoRepository tacoRepo;
 
